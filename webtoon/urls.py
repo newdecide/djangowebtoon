57a350cf667ec34f8f7fd12abb8fbfd52616ce44
newdecide <liberty-1@nate.com>
2019-11-14 13:12:55 +0900

    WebtoonList 클래스 생성 및 urls.py 클래스 등록

--- webtoon/urls.py
+++ webtoon/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('', WebtoonList.as_view(), name='list'),
     path('naver_crw', naver_webtoon_crw)
 ]
--- webtoon/views.py
+++ webtoon/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .naver_webtoon import *
+from django.views.generic import ListView
 
 # Create your views here.
 def naver_webtoon_crw(request):
     naver_webtoon()
-    return HttpResponse("네이버 웹툰 크롤릴 완료")
+    return HttpResponse("네이버 웹툰 크롤링 완료")
+
+
+class WebtoonList(ListView):
+    model = WebToon
