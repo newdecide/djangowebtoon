04a2f84431645a0f4e1752335edba7dbce8eda94
newdecide <liberty-1@nate.com>
2019-11-14 11:14:07 +0900

    naver_webtoon.py - 제목, url, 저자 크롤링

--- webtoon/naver_webtoon.py
+++ webtoon/naver_webtoon.py
@@ -12,5 +12,8 @@ webtoon_list = bs_object.find('div' , {'class': 'list_area daily_img'})
 webtoon_list_tags = webtoon_list.findAll('li')
 
 for webtoon_tag in webtoon_list_tags:
-    # 반복문을오 제목만 가져온다.
+    # 반복문으로 제목만 가져온다.
+    # 함수로 만듦(월~일 웹툰)
     print(webtoon_tag.find('a')['title'])
+    print(webtoon_tag.find('a').find('img')['src'])
+    print(webtoon_tag.find('dd',{'class':'desc'}).text.strip())
