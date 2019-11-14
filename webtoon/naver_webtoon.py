3692198524d7ca8a650e5e3e46d1506fd25fa318
newdecide <liberty-1@nate.com>
2019-11-14 12:54:59 +0900

    webtoon DB 등록 코드 작성 및 views.py 작성

--- webtoon/naver_webtoon.py
+++ webtoon/naver_webtoon.py
@@ -1,5 +1,6 @@
 import requests
 import bs4
+from .models import WebToon
 
 # html = requests.get("https://comic.naver.com/webtoon/weekdayList.nhn?week=mon")
 # print("status code:", html.status_code)
@@ -28,10 +29,20 @@ def naver_webtoon_day(day):
     webtoon_list_tags = webtoon_list.findAll('li')
 
     for webtoon_tag in webtoon_list_tags:
-        # 반복문으로 제목만 가져온다.
-        print(webtoon_tag.find('a')['title'])
-        print(webtoon_tag.find('a').find('img')['src'])
-        print(webtoon_tag.find('dd',{'class':'desc'}).text.strip())
+        webtoon = WebToon()
+        webtoon.site_name = "네이버"
+        webtoon.webtoon_name = webtoon_tag.find('a')['title']
+        webtoon.webtoon_author = webtoon_tag.find('dd',{'class':'desc'}).text.strip()
+        webtoon.webtoon_img_url = webtoon_tag.find('a').find('img')['src']
+        webtoon.webtoon_id = "네이버_" + webtoon_tag.find('a')['title']
+
+        webtoon.save()
+
+        # print(webtoon_tag.find('a')['title'])
+        # print(webtoon_tag.find('a').find('img')['src'])
+        # print(webtoon_tag.find('dd',{'class':'desc'}).text.strip())
+
+
 
 # 장고실행시 실행되버리기때문에 함수로 만듦.
 def naver_webtoon():
--- webtoon/views.py
+++ webtoon/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .naver_webtoon import *
 
 # Create your views here.
+def naver_webtoon_crw(request):
+    naver_webtoon()
+    return HttpResponse("네이버 웹툰 크롤릴 완료")
