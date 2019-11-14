3dd7d61a954b7f6737302a327a10dad996b2bef4
newdecide <liberty-1@nate.com>
2019-11-14 11:27:53 +0900

    webtoon - naver_webtoon_day 함수로 변환

--- webtoon/naver_webtoon.py
+++ webtoon/naver_webtoon.py
@@ -1,19 +1,40 @@
 import requests
 import bs4
 
-html = requests.get("https://comic.naver.com/webtoon/weekdayList.nhn?week=mon")
+# html = requests.get("https://comic.naver.com/webtoon/weekdayList.nhn?week=mon")
 # print("status code:", html.status_code)
 # print(html.text)
 
-bs_object = bs4.BeautifulSoup(html.text, "html.parser")
+# bs_object = bs4.BeautifulSoup(html.text, "html.parser")
 # print(bs_object)
-webtoon_list = bs_object.find('div' , {'class': 'list_area daily_img'})
+# webtoon_list = bs_object.find('div' , {'class': 'list_area daily_img'})
 # print(webtoon_list)
-webtoon_list_tags = webtoon_list.findAll('li')
+# webtoon_list_tags = webtoon_list.findAll('li')
 
-for webtoon_tag in webtoon_list_tags:
+# for webtoon_tag in webtoon_list_tags:
     # 반복문으로 제목만 가져온다.
     # 함수로 만듦(월~일 웹툰)
-    print(webtoon_tag.find('a')['title'])
-    print(webtoon_tag.find('a').find('img')['src'])
-    print(webtoon_tag.find('dd',{'class':'desc'}).text.strip())
+    # print(webtoon_tag.find('a')['title'])
+    # print(webtoon_tag.find('a').find('img')['src'])
+    # print(webtoon_tag.find('dd',{'class':'desc'}).text.strip())
+
+
+def naver_webtoon_day(day):
+    html = requests.get("https://comic.naver.com/webtoon/weekdayList.nhn?week="+day)
+    bs_object = bs4.BeautifulSoup(html.text, "html.parser")
+    # print(bs_object)
+    webtoon_list = bs_object.find('div', {'class': 'list_area daily_img'})
+    # print(webtoon_list)
+    webtoon_list_tags = webtoon_list.findAll('li')
+
+    for webtoon_tag in webtoon_list_tags:
+        # 반복문으로 제목만 가져온다.
+        # 함수로 만듦(월~일 웹툰)
+        print(webtoon_tag.find('a')['title'])
+        print(webtoon_tag.find('a').find('img')['src'])
+        print(webtoon_tag.find('dd',{'class':'desc'}).text.strip())
+
+
+week_list = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+for week in week_list:
+    naver_webtoon_day(week)
