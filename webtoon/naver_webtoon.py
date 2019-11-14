e80b6f11c223a6f5670a2a5879655afccd34772a
newdecide <liberty-1@nate.com>
2019-11-14 10:50:59 +0900

    webtoon - 크롤링 월요 웹툰 제목 가져오기까지

--- webtoon/naver_webtoon.py
+++ webtoon/naver_webtoon.py
@@ -1,6 +1,16 @@
 import requests
+import bs4
 
 html = requests.get("https://comic.naver.com/webtoon/weekdayList.nhn?week=mon")
-print("status code:", html.status_code)
-print(html.text)
+# print("status code:", html.status_code)
+# print(html.text)
 
+bs_object = bs4.BeautifulSoup(html.text, "html.parser")
+# print(bs_object)
+webtoon_list = bs_object.find('div' , {'class': 'list_area daily_img'})
+# print(webtoon_list)
+webtoon_list_tags = webtoon_list.findAll('li')
+
+for webtoon_tag in webtoon_list_tags:
+    # 반복문을오 제목만 가져온다.
+    print(webtoon_tag.find('a')['title'])
