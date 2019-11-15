9ac3f1062937a9e54b3bcd95579b43da38034f52
newdecide <liberty-1@nate.com>
2019-11-15 13:48:04 +0900

    webtoon - 상세화면 구현

--- webtoon/views.py
+++ webtoon/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .naver_webtoon import *
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .daum_webtoon import *
 
 # Create your views here.
@@ -36,3 +36,7 @@ class DaumWebtoonList(ListView):
 
     def get_queryset(self):
         return WebToon.objects.filter(site_name='다음')
+
+class WebtoonDetailView(DetailView):
+    model = WebToon
+
